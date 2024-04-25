0419e796de5846ea12d761b7bfe31799a608a896
Lisandro Pintos <licha.pintos@gmail.com>
2024-04-25 19:24:49 -0300

    optional user creation

--- iac/writer/iam.tf
+++ iac/writer/iam.tf
@@ -1,3 +1,8 @@
+resource "aws_iam_user" "caller_user" {
+  count = length(data.aws_iam_user.caller_user.user_name) == 0 ? 1 : 0
+  name = var.username
+}
+
 resource "aws_iam_policy" "s3_writer_policy" {
   name        = "S3WriterPolicy"
   description = "Policy for cross-account S3 write access"
@@ -9,7 +14,8 @@ resource "aws_iam_policy" "s3_writer_policy" {
         Action = [
           "s3:PutObject",
           "s3:GetObject",
-          "s3:ListBucket"
+          "s3:ListBucket",
+          "s3:DeleteObject",
         ]
         Effect   = "Allow"
         Resource = [
@@ -21,6 +27,6 @@ resource "aws_iam_policy" "s3_writer_policy" {
 }
 
 resource "aws_iam_user_policy_attachment" "s3_writer_user_attach" {
-  user       = data.aws_iam_user.caller_user.user_name
+  user       = data.aws_iam_user.caller_user.user_name == null ? aws_iam_user.caller_user.name : data.aws_iam_user.caller_user.user_name
   policy_arn = aws_iam_policy.s3_writer_policy.arn
 }
